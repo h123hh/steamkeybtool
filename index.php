bc0cea41f0fc9870278d3508b0f904a7c9baa55d
h123hh <h13860258331@outlook.com>
2020-09-04 19:40:15 +0800

    修复BUG

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="author" content="">
-<link rel="icon" href="">
+<link rel="icon" href="favicon.ico">
 <title>SteamKey生成工具</title>
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
